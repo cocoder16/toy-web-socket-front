2a8f87e298e205030de51456927d78137ce8c764
cocoder16 <cocoder16@gmail.com>
2021-05-09 15:48:17 +0900

    Update typescript version on vscode
    Update tsconfig

---
+++ src/components/logIn/logIn.test.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { render } from "@testing-library/react";
+
+// import LogIn from "components/logIn";
+
+// describe("<Counter />", () => {
+//   const userName = "dummy";
+
+//   it("renders todos properly", () => {
+//     const { getByText } = render(<LogIn userName={userName} />);
+//     getByText(userName.text); // 닉네임 입력 텍스트
+//     // 버튼 렌더
+//   });
+// });
