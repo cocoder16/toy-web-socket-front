31708eb8123557a0c886880dacd438e92a4d0c93
cocoder16 <cocoder16@gmail.com>
2021-05-16 12:30:33 +0900

    [Test] Add go log in btn

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,13 +5,18 @@ import App from "src/App";
 describe("App", () => {
   const { getByTestId } = render(<App />);
 
-  it("click skip btn => render chat room", () => {
-    const logInComponent = getByTestId("log-in");
+  it("click skip btn => render chat room & click rename btn => render log in", () => {
+    let logInComponent = getByTestId("log-in");
     const skipBtn = getByTestId("skip-btn");
     expect(logInComponent).toBeInTheDocument();
 
     fireEvent.click(skipBtn);
     const chatRoomComponent = getByTestId("chat-room");
+    const goLogInBtn = getByTestId("go-log-in-btn");
     expect(chatRoomComponent).toBeInTheDocument();
+
+    fireEvent.click(goLogInBtn);
+    logInComponent = getByTestId("log-in");
+    expect(logInComponent).toBeInTheDocument();
   });
 });
