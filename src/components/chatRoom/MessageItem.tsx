0bc461ef166d3f3926181c4d8504fbf2b0890ffe
cocoder16 <cocoder16@gmail.com>
2021-12-13 15:49:01 +0900

    [Add] dayjs

--- src/components/chatRoom/MessageItem.tsx
+++ src/components/chatRoom/MessageItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import dayjs from "dayjs";
 
 type IProps = {
   message: IMessage;
@@ -11,7 +12,7 @@ function MessageItem({ message }: IProps) {
     <div className="d-flex flex-row">
       {nickname && <div className="message-nickname">{nickname}: </div>}
       <div>{content}</div>
-      <div className="time">{time}</div>
+      <div className="time">{dayjs(time).format("HH:mm")}</div>
     </div>
   );
 }
